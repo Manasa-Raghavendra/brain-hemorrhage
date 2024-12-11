bea12353ffb72fa58e6c7eac868a8c9544302a40
Manasa-Raghavendra <manasabr0708@gmail.com>
2024-12-11 16:42:14 +0530

    added

--- app.py
+++ app.py
@@ -96,7 +96,8 @@ def predict():
         print(f"Error in /predict route: {e}")
         flash('An unexpected error occurred')
         return redirect(url_for('home'))
-
+    
+#updated
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
